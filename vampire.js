39c5d619bf3f319845d64be1c2ba3c2efc19e2a4
Kim O'Dell <kimbo.odell@gmail.com>
2024-04-12 19:16:24 -0400

    Add vampire.js queries

--- vampire.js
+++ vampire.js
@@ -10,22 +10,40 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    let totalOffspring = 0;
 
+    this.offspring.forEach((offspring) => {
+      totalOffspring++
+    });
+
+    return totalOffspring;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let currentVamp = this;
+
+    //if the current vamp has a valid creator
+    while (currentVamp.creator) {
+      //update the current vamp to the create of the next vamp in the line and increase the count
+      currentVamp = currentVamp.creator;
+      numberOfVamps++;
+    }
+
+    return numberOfVamps;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal  
   }
 
   /** Stretch **/
